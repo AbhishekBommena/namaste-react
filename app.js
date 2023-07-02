dadd3a99067dd65011eb8f73b2c4bddfffcff809
Abhishek Bommena <abhishek.bommena22@gmail.com>
2023-07-02 09:22:13 +0530

    episode-1

---
+++ app.js
@@ -0,0 +1,19 @@
+// const heading = React.createElement("h1", {id:"heading",className:"headingContent"}, "This is React!");
+// console.log(heading);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// console.log(root)
+// root.render(heading);
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I am a h1 tag"),
+    React.createElement("h2", {}, "I am a h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h3", {}, "I am a h3 tag"),
+    React.createElement("h4", {}, "I am a h4 tag"),
+  ]),
+]);
+console.log(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
