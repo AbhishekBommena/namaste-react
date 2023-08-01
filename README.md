9660daeaf178bc4c339355b4cf3e50be073ceb08
Abhishek Bommena <abhishek.bommena22@gmail.com>
2023-08-01 19:05:46 +0530

    Episode - 3 assignment added in readMe.md

--- README.md
+++ README.md
@@ -3,3 +3,31 @@ This repo is all about React course that I am doing.
 
 # Namaste-React webseries by AKSHAY SAINI.
 check out : https://namastedev.com/namaste-react/ for this course.
+
+
+# Assignment - 3
+-----------------------------
+
+1. What is JSX ?
+➡ JSX is not HTML in Javascript.
+➡ JSX is a syntax. It is HTML like syntax OR XML like syntax.
+➡ JSX is different , React is different.
+For ex : const heading = React.createElement("h1",{},"Core React");
+    Basically this is core react way of creating react element.
+    whereas , const heading = <h1>JSX way</h1>;
+    This is JSX way of creating react element.
+
+
+➡ JS engine don't understand JSX.
+➡ JSX is transpiled before it reaches the JS Engine by parcel(Not itself but babel will do that). 
+➡ So what happens is, JSX => React.createElement() => React element which is JS Object => Html element.
+➡ All this conversion is done by Babel.
+
+2. Super Power of JSX 
+➡ Inside JSX Javascript can also be used with curly braces.
+➡ JSX prevents cross-site scripting attacks(when data coming from a api and it is malicious jsx won't allow).
+
+3. Composition of Component
+➡ Basically it means component inside another component.
+
+4. {TitleComponent()} and {<TitleComponent/>} and {<TitleComponent><TitleComponent/>} => everything is same.
--- app.js
+++ app.js
@@ -5,17 +5,66 @@
 // root.render(heading);
 
 import React from "react";
-import ReactDOM  from "react-dom/client";
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am a h1 tag"),
-    React.createElement("h2", {}, "I am a h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h3", {}, "I am a h3 tag"),
-    React.createElement("h4", {}, "I am a h4 tag"),
-  ]),
-]);
-console.log(parent)
+import ReactDOM from "react-dom/client";
+
+// Below is Javascript way of creating react element with children
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child1" }, [
+//     React.createElement("h1", {}, "I am a h1 tag"),
+//     React.createElement("h2", {}, "I am a h2 tag"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h3", {}, "I am a h3 tag"),
+//     React.createElement("h4", {}, "I am a h4 tag"),
+//   ]),
+// ]);
+// console.log(parent)
+
+// Below is JSX way of creating react element
+// const jsxHeading = (
+//   <div className="parent">
+//     <div className="child1">
+//       <h1>I am a h1 tag</h1>
+//       <h2>I am a h2 tag</h2>
+//     </div>
+//     <div className="child2">
+//       <h3>I am a h3 tag</h3>
+//       <h4>I am a h4 tag</h4>
+//     </div>
+//   </div>
+// );
+// console.log(jsxHeading)
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(jsxHeading);
+
+// Below is creating React Component using js functions.
+const TitleComponent = () => <h1>Namaste React Web Series 🚀</h1>;
+
+const HeadingComponent = () => (
+  <div className="container">
+    {TitleComponent()}
+    <TitleComponent></TitleComponent>
+    <TitleComponent />
+    <h1>Functional Component</h1>
+  </div>
+);
+
+const HeaderComponent = () => {
+  return (
+    <div className="headerContainer">
+      <a href="#" className="logo">
+        <i class="fa-brands fa-google fa-xl googleIcon"></i>
+        <p className="googleText">oogle</p>
+      </a>
+      <div className="searchFieldContainer">
+        <input type="search" className="searchField" />
+        <a href="#"><i class="fa-solid fa-magnifying-glass searchIcon"></i></a>
+      </div>
+      <a href="#" className="userIcon">
+        <i class="fa-regular fa-circle-user fa-xl"></i>
+      </a>
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeaderComponent />);
